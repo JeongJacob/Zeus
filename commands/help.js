9193af59daf629ac708e90440b6182563d9ef901
정세민 <99317339+JeongJacob@users.noreply.github.com>
2026-03-05 07:46:21 +0000

    Feat: 명령어 추가 설명

--- commands/help.js
+++ commands/help.js
@@ -1,22 +1,26 @@
 const { SlashCommandBuilder } = require("@discordjs/builders");
 
 module.exports = {
-    data: new SlashCommandBuilder().setName("도움말").setDescription("사용 가능한 봇 명령어를 안내합니다."),
-    async execute(interaction) {
-        return interaction.reply(
-            "**🤖 사용 가능한 명령어 안내:**\n\n" +
-                "`/id <Riot ID>`\n" +
-                "➤ 해당 라이엇 계정의 PUUID를 조회합니다.\n" +
-                "예시: `/id riotid: Radiohead#KR97`\n\n" +
-                "`/전적 <puuid>`\n" +
-                "➤ 최근 한 판의 전체 게임 정보를 조회합니다.\n" +
-                "예시: `/전적 puuid: abc1234...`\n\n" +
-                "`/생성`\n" +
-                "➤ 롤 파티 모집을 시작합니다. (포지션 선택, 시작 시간 입력 등 가능)\n\n" +
-                "`/내전`\n" +
-                "➤ 롤 내전 모집을 시작합니다. (10명 인원 제한, 시작 시간 입력 등 가능))\n\n" +
-                "`/도움말`\n" +
-                "➤ 이 도움말 메시지를 다시 보여줍니다."
-        );
-    },
+  data: new SlashCommandBuilder()
+    .setName("도움말")
+    .setDescription("사용 가능한 봇 명령어를 안내합니다."),
+  async execute(interaction) {
+    return interaction.reply(
+      "**🤖 사용 가능한 명령어 안내:**\n\n" +
+        "`/id <Riot ID>`\n" +
+        "➤ 해당 라이엇 계정의 PUUID를 조회합니다.\n" +
+        "예시: `/id riotid: Radiohead#KR97`\n\n" +
+        "`/전적 <puuid>`\n" +
+        "➤ 최근 한 판의 전체 게임 정보를 조회합니다.\n" +
+        "예시: `/전적 puuid: abc1234...`\n\n" +
+        "`/자랭`\n" +
+        "➤ 롤 자랭 파티 모집을 시작합니다. (포지션 선택, 시작 시간 입력 등 가능)\n\n" +
+        "`/내전`\n" +
+        "➤ 롤 내전 모집을 시작합니다. (10명 인원 제한, 시작 시간 입력 등 가능)\n\n" +
+        "`/지우개`\n" +
+        "➤ 작성된 글을 지웁니다. (1~100개 갯수 입력 가능)\n\n" +
+        "`/도움말`\n" +
+        "➤ 이 도움말 메시지를 다시 보여줍니다.",
+    );
+  },
 };
